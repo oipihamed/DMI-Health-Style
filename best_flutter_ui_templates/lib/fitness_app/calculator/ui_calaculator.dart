22c6e78c31c7762d8dc6a76e9a539d35f9e63330
Sebas-B <sebedrmz1600@gmail.com>
2022-10-21 18:23:59 -0500

    Calculadora IMC

---
+++ best_flutter_ui_templates/lib/fitness_app/calculator/ui_calaculator.dart
@@ -0,0 +1,82 @@
+import 'package:best_flutter_ui_templates/fitness_app/calculator/widgets/widgets_calculator.dart';
+import "package:flutter/material.dart";
+
+import '../fitness_app_theme.dart';
+
+class CalculatorUI extends StatefulWidget {
+  const CalculatorUI({super.key});
+
+  @override
+  State<CalculatorUI> createState() => _CalculatorUIState();
+}
+
+class _CalculatorUIState extends State<CalculatorUI> {
+  final TextEditingController _pesoController = TextEditingController();
+  final TextEditingController _alturaController = TextEditingController();
+  String respuesta = "Respuesta";
+  double imc = 0.0;
+  late String rango;
+
+  void _calcular() {
+    setState(() {
+      double peso = double.parse(_pesoController.text);
+      double altura = double.parse(_alturaController.text)/100;
+      FitnessAppTheme.nearlyBlack;
+      imc = peso / (altura * altura);
+      
+      if (imc < 18.5) {
+        rango = "Peso bajo";
+      } else if(imc >= 18.6 && imc <= 24.9) {
+        rango = "Peso normal ";
+      } else if(imc >= 25 && imc <= 29.9){
+        rango = "Sobrepeso ";
+      } else if(imc >= 30 && imc <= 34.9){
+        rango = "Obesidad leve ";
+      } else if(imc >= 35 && imc <= 39.9){
+        rango = "Obesidad media ";
+      } else if(imc > 40){
+        rango = "Obesidad morbida ";
+      }
+
+      respuesta = "TU IMC es: "+imc.toStringAsFixed(1) + ", estas en: " + rango;
+
+    });
+  }
+
+  Widget _sizedEspacio() {
+    return const SizedBox(
+      height: 30,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Calculadora IMC")),
+      ),
+      body: SingleChildScrollView(
+            child: MyContainer(
+              child: Column(
+                children: [
+                  Stack(
+                    children: const [MyAppContainer()],
+                  ),
+                  MyTextInput(
+                      inputController: _pesoController, label: "Peso en Kg"),
+                  MyTextInput(
+                      inputController: _alturaController, label: "Altura en Cm"),
+                  _sizedEspacio(),
+                  MyText(text: respuesta.toString()),
+                  _sizedEspacio(),
+                  MyButton(
+                      lblText: const Text("Calcular"), press: (() => _calcular())),
+                  _sizedEspacio(),
+                  MyTable(),
+                ],
+              ),
+            ),
+      ),
+    );
+  }
+}
